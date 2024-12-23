ac070c1ab73b5d61562ef25fd6c7fb8a7c96e795
Noorul Ashfaq A <noorulashfaq786@gmail.com>
2024-12-23 17:57:56 +0530

    Removed AppModule from app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
+// import { ApplicationConfig } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+
+// import { routes } from './app.routes';
+// import { provideClientHydration } from '@angular/platform-browser';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [provideRouter(routes), provideClientHydration()]
+// };
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import { routes } from './app.routes';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+  ],
 };
+
